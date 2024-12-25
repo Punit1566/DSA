1339bf16a0bef3afe274ac46e89c5a2fd81d7e55
Punit1566 <125143948+Punit1566@users.noreply.github.com>
2024-12-26 03:25:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (6.26%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(int n) {
         int bit_cnt=0;
         for(int i=0;i<32;i++){
-            if(n&(1<<i))bit_cnt++;
+            if((n>>i)&1)bit_cnt++;
         }
         return bit_cnt;
     }
