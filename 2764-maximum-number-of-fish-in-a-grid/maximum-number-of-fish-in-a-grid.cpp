627a1f0c3f2ec1c1be73bc51cee3cce5f2ac9290
Punit Singh <125143948+Punit1566@users.noreply.github.com>
2025-01-28 22:30:19 +0530

    Time: 83 ms (6.66%) | Memory: 135.5 MB (5.08%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -29,10 +29,28 @@ public:
         int m=grid.size();
         int n=grid[0].size();
         int ans=0;
+        vector<vector<int>>vis(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]>0){
+                if(grid[i][j]>0&&!vis[i][j]){
                     ans=max(ans,solve(i,j,grid));
+                    vis[i][j]=1;
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    vector<pair<int,int>>nbr={{0,1},{0,-1},{-1,0},{1,0}};
+                    while(!q.empty()){
+                        int row=q.front().first;
+                        int col=q.front().second;
+                        q.pop();
+                        for(auto x:nbr){
+                            int nrow=row+x.first;
+                            int ncol=col+x.second;
+                            if(nrow>=0 && nrow<m&&ncol>=0 && ncol<n && !vis[nrow][ncol] && grid[nrow][ncol]>0){
+                                q.push({nrow,ncol});
+                                vis[nrow][ncol]=1;
+                            }
+                        }
+                    }
                 }
             }
         }
