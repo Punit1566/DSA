4e0bae5262a3e5bfe3e4dda37750d28ead5447b1
Punit Singh <125143948+Punit1566@users.noreply.github.com>
2025-02-07 02:26:23 +0530

    Time: 347 ms (26.75%) | Memory: 210.4 MB (18.73%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,15 +1,45 @@
+class DSU{
+    public:
+    vector<int>parent,size;
+    DSU(int n){
+        size.resize(n,1);
+        parent.resize(n);
+        iota(parent.begin(),parent.end(),0);
+    }
+    int findUParent(int n){
+        if(parent[n]==n)return n;
+        return findUParent(parent[n]);
+    }
+    void UnionBySize(int u,int v){
+        int ulp_u=findUParent(u);
+        int ulp_v=findUParent(v);
+
+        if(ulp_u==ulp_v)return;
+
+        if(size[ulp_u]>size[ulp_v]){
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }else if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
+
 class Solution {
 public:
-    void solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&vis,
-    unordered_map<int,int>&unq_id,int curr_id){
-        int n=grid.size();
+    bool isValid(int n,int row,int col){
+        return row>=0 && row<n && col>=0 && col<n;
+    }
+    void BFS(int i,int j,vector<vector<int>>& grid,vector<vector<int>>&vis, DSU &d){
         queue<pair<int,int>>q;
+        int n=grid.size();
         q.push({i,j});
         vis[i][j]=1;
-        vector<pair<int,int>>used;
-        used.push_back({i,j});
         vector<pair<int,int>>nbr={{0,-1},{0,1},{-1,0},{1,0}};
-        int sz=1;
         while(!q.empty()){
             int row=q.front().first;
             int col=q.front().second;
@@ -17,61 +47,51 @@ public:
             for(auto &x:nbr){
                 int nrow=row+x.first;
                 int ncol=col+x.second;
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<n && grid[nrow][ncol]==1&&vis[nrow][ncol]==0){
+                if(isValid(n,nrow,ncol) && grid[nrow][ncol]==1 && !vis[nrow][ncol]){
                     q.push({nrow,ncol});
                     vis[nrow][ncol]=1;
-                    sz++;
-                    used.push_back({nrow,ncol});
+                    int u=row*n+col;
+                    int v=nrow*n+ncol;
+                    d.UnionBySize(u,v);
                 }
             }
         }
-        for(auto x:used){
-            vis[x.first][x.second]=curr_id;
-        }
-        unq_id[curr_id]=sz;
     }
     int largestIsland(vector<vector<int>>& grid) {
         int n=grid.size();
-        // vector<vector<int>>updated(n,vector<int>(n,0));
-        unordered_map<int,int>unq_id;
+        DSU d(n*n);
         vector<vector<int>>vis(n,vector<int>(n,0));
-        int curr_id=2;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1&&vis[i][j]==0){
-                    solve(i,j,grid,vis,unq_id,curr_id);
-                    curr_id++;
+        for(int row=0;row<n;row++){
+            for(int col=0;col<n;col++){
+                if(grid[row][col]==1&&!vis[row][col]){
+                    BFS(row,col,grid,vis,d);
                 }
             }
         }
-        
         int maxi=0;
-        vector<pair<int,int>>nbr={{-1,0},{1,0},{0,-1},{0,1}};
-        // int left_down,down_right,right_up,up_left;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0){
-                    int ans=0;
+        vector<pair<int,int>>nbr={{0,-1},{0,1},{-1,0},{1,0}};
+        for(int row=0;row<n;row++){
+            for(int col=0;col<n;col++){
+                if(grid[row][col]==0){
                     unordered_set<int>st;
-                    for(auto x:nbr){
-                        int row=i+x.first;
-                        int col=j+x.second;
-                        if(row>=0 && row<n && col>=0 && col<n && grid[row][col]==1){
-                            int id=vis[row][col];
-                            if(st.find(id)==st.end()){
-                                ans+=unq_id[id];
-                                st.insert(id);
+                    int ans=0;
+                    for(auto &x:nbr){
+                        int nrow=row+x.first;
+                        int ncol=col+x.second;
+                        if(isValid(n,nrow,ncol)&&grid[nrow][ncol]==1){
+                            int idx=nrow*n+ncol;
+                            int ulp_idx=d.findUParent(idx);
+                            if(st.find(ulp_idx)==st.end()){
+                                ans+=d.size[ulp_idx];
+                                st.insert(ulp_idx);
                             }
                         }
                     }
-                    maxi=max(maxi,ans);
+                    maxi=max(maxi,ans+1);
                 }
             }
         }
-        if(maxi==0){
-            if(grid[0][0]==1)return n*n;
-        }
-        return maxi+1;
-        
+        if(maxi==0)return n*n;
+        return maxi;
     }
 };
